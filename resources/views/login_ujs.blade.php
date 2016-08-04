5acaa9d55d0f23be91e647c403ebb3203ec309f3
rodrigore <rodrigopcg@gmail.com>
2016-08-04 15:54:28 -0400

    Add disable with

--- resources/views/login_ujs.blade.php
+++ resources/views/login_ujs.blade.php
@@ -40,7 +40,7 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" data-disable-with="<i class='fa fa-spinner fa-spin'></i> Login">
                                     <i class="fa fa-btn fa-sign-in"></i> Login
                                 </button>
                             </div>
